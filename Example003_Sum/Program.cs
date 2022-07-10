4c8d4141bf604695582f02a0204a0e5f7cc38ce8
Hmuriy Monakh <max_08931993@mail.ru>
2022-07-10 14:46:52 +0300

    add Sum 013

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+﻿//int numberA = 35;
+//int numberB = 5;
+//int result = numberA + numberB;
+//Console.WriteLine(result );
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
